d1d155ddd9e3b64d6187753aaabf4be89c678cbd
MaetDol <rndka9@gmail.com>
2022-05-16 02:13:52 +0900

    feat, `readDirectoriesAsFullPath` 구현
    테스트 코드도 동작하게 수정했습니다

--- src/libs/fileSystem.js
+++ src/libs/fileSystem.js
@@ -1,10 +1,38 @@
 import fs from 'node:fs';
+import path from 'node:path';
 
 /**
  *
- * @param {string} path
+ * @param {string} absolutePath absolute path for read directories
  * @returns {string[]} sub-directory's absolute paths
  */
-export function readDirectoriesAsFullPath(path) {
-  return fs.readFileSync(path);
+export function readDirectoriesAsFullPath(absolutePath) {
+  const resolveToAbsolutePath = (p) => path.resolve(absolutePath, p);
+  const contents = fs.readdirSync(absolutePath);
+  return contents
+    .filter((name) => pathIsDirectory(resolveToAbsolutePath(name)))
+    .map(resolveToAbsolutePath);
+}
+
+/**
+ *
+ * @param {string} absolutePath
+ * @returns {boolean}
+ */
+export function pathIsDirectory(absolutePath) {
+  try {
+    return fs.lstatSync(absolutePath).isDirectory();
+  } catch (e) {
+    return false;
+  }
+}
+
+/**
+ *
+ * @param {string} absolutePath
+ */
+export function createDirectory(absolutePath) {
+  try {
+    fs.mkdirSync(absolutePath);
+  } catch (e) {}
 }
--- src/libs/fileSystem.test.js
+++ src/libs/fileSystem.test.js
@@ -1,21 +1,23 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { readDirectoriesAsFullPath } from './fileSystem';
+import { createDirectory, readDirectoriesAsFullPath } from './fileSystem';
 
-test(`Test 'readDirectoriesAsFullPath' function`, () => {
+test(`Test 'readDirectoriesAsFullPath' function`, async () => {
   const TEST_WORKDIR = 'fileSystemTest';
   const currentPath = (p) => path.resolve(__dirname, TEST_WORKDIR, p);
-  fs.mkdir(currentPath('./'));
+  createDirectory(currentPath('./'));
 
   const directories = ['a', 'b.c', 'c.txt', '.github'];
-  directories.forEach((dir) => fs.mkdir(currentPath(dir)));
+  directories.forEach((dir) => createDirectory(currentPath(dir)));
 
   const files = ['ab', 'b', 'temp.txt', '.gitignore'];
-  files.forEach((file) => fs.writeFileSync(currentPath(file)));
+  files.forEach((file) => fs.writeFileSync(currentPath(file), ''));
 
   const resultOfTargetFunction = readDirectoriesAsFullPath(currentPath('./'));
-  expect(resultOfTargetFunction).toEqual(directories);
+  const expectDirectories = directories.map(currentPath);
+  // 순서에 따라 다른 결과가 나오므로, 정렬이 필요합니다
+  expect(resultOfTargetFunction.sort()).toEqual(expectDirectories.sort());
 
   // cleanup
-  fs.rmdirSync(currentPath('./'));
+  fs.rmdirSync(currentPath('./'), { recursive: true });
 });
