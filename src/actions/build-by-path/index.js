7a4508e5749c179b63bc60c6c2493fd19a6d6d5c
MaetDol <rndka9@gmail.com>
2022-05-22 22:38:30 +0900

    chore, 로깅 추가

--- src/actions/build-by-path/index.js
+++ src/actions/build-by-path/index.js
@@ -1,8 +1,22 @@
-import { runBuildCommandAt } from '../../libs/projectExplorer';
+import { getPackageJson, runBuildCommandAt } from '../../libs/projectExplorer';
 import * as core from '@actions/core';
 
 run(core.getInput('fullpaths'));
 
 export function run(fullpaths) {
-  fullpaths.forEach(runBuildCommandAt);
+  const buildPath = (fullpath) => {
+    const packageJson = getPackageJson(fullpath);
+    if (!packageJson) {
+      console.log('package.json Not found. exit.');
+      return;
+    }
+
+    runBuildCommandAt(fullpath);
+  };
+
+  fullpaths.forEach((fullpath) => {
+    core.startGroup(`Build ${fullpath}`);
+    buildPath(fullpath);
+    core.endGroup();
+  });
 }
